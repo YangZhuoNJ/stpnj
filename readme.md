a68e3204be2b855df3babd59d70860da5e94cef6
NJ <NJ>
2016-11-12 17:14:25 +0800

    add database set

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 Hello world Spring!
+
+# mysql server root password
+mysql root@localhost :  hEEUNqiAe7.5
+mysql root@localhost :  123456
---
+++ src/main/java/com/nj/tampa/server/DataBaseConfig.java
@@ -0,0 +1,11 @@
+package com.nj.tampa.server;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by admin on 2016/11/12.
+ */
+
+@Configuration
+public class DataBaseConfig {
+}
