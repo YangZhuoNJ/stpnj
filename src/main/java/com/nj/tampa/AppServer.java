b5f53fa9655c1c0fada2f0bed45fbc1329a59930
NJ_home <hityangzhuo@163.com>
2016-11-15 21:57:09 +0800

    update

--- src/main/java/com/nj/tampa/AppServer.java
+++ src/main/java/com/nj/tampa/AppServer.java
@@ -4,12 +4,11 @@ package com.nj.tampa;
  * Created by admin on 2016/11/11.
  */
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.nj.tempa.mapper")
+//@MapperScan(basePackages = "com.nj.tempa.mapper")
 public class AppServer {
 
     public static void main(String[] args) {
